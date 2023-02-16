8c376859b88dd46a267a75fbc344aee4cd57070d
lifegi <117684753+lifegi@users.noreply.github.com>
2023-02-16 14:21:52 +0500

    first commit

---
+++ src/component/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        <h1>this a news component</h1>
+      </div>
+    );
+  }
+}
+
+export default News;
